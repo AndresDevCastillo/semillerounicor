28895e78cbe67501d6664af0b13d55d739aaf09f
JoseDanielDurango <jdurangocaballero59@correo.unicordoba.edu.co>
2024-05-27 11:36:48 -0500

    Prueba incial de pagina y componentes de prospectivas de semillero

---
+++ components/JdProspectiva.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Prospectiva de Semillero</h2>
+    </div>
+</template>
---
+++ pages/jdprospectiva.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Prospectiva de Semillero</h1>
+        <JdProspectiva />
+    </div>
+</template>
