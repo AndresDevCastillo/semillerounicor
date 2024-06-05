7464fa696ccad3ee995589e9561524b00396af1a
JoseDanielDurango <jdurangocaballero59@correo.unicordoba.edu.co>
2024-06-05 16:30:03 -0500

    Changes

---
+++ components/JdGrupo.vue
@@ -0,0 +1,12 @@
+<script setup>
+const grupo = {
+    nombre: "EduTLan",
+    facultad: "Educacion y Ciencias Humanas",
+    logo: "~/data/logos/Logo1.jpg",
+    semilleros: [
+        {
+            
+        }
+    ]
+}
+</script>
