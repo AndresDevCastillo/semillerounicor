f39b84be84d389dfe5b2d2f8b9170f35cdf74924
Khaai01 <143374970+Khaai01@users.noreply.github.com>
2024-06-05 17:40:25 -0500

    change
    changes

--- components/jrplaneacion.vue
+++ components/jrplaneacion.vue
@@ -1,7 +1,11 @@
 <script setup>
-import semillero from '../data/Semillero.json'
+
+const props = defineProps({
+  semillero: Object
+})
 
 </script>
+
 <template>
     <div class="planeacion">
         <div class="card">
@@ -10,16 +14,49 @@ import semillero from '../data/Semillero.json'
                     <TabView>
                         <TabPanel header="Estrategias">
                             <Accordion>
-                                <AccordionTab v-for="estrategia in semillero[0].estrategias" :header="estrategia">
-                                    <p class="m-0">
-                                        Sin ningun texto
-                                    </p>
+                                <AccordionTab header="Misión">
+                                    <p class="m-0">{{ semillero.mision }}</p>
                                 </AccordionTab>
-                               
+                                <AccordionTab header="Visión">
+                                    <p class="m-0">{{ semillero.vision }}</p>
+                                </AccordionTab>
+                                <AccordionTab header="Objetivos">
+                                    <Accordion>
+                                        <AccordionTab header="Objetivo General">
+                                            <p class="m-0">{{ semillero.objetivoGeneral }}</p>
+                                        </AccordionTab>
+                                        <AccordionTab header="Objetivos Específicos">
+                                            <ul>
+                                                <li v-for="objetivo in semillero.objetivosEspecificos" :key="objetivo">
+                                                    {{ objetivo }}
+                                                </li>
+                                            </ul>
+                                        </AccordionTab>
+                                    </Accordion>
+                                </AccordionTab>
+                                <AccordionTab header="Actividades">
+                                    <ul>
+                                        <li v-for="actividad in semillero.actividades" :key="actividad">
+                                            {{ actividad }}
+                                        </li>
+                                    </ul>
+                                </AccordionTab>
+                                <AccordionTab header="Proyectos">
+                                    <ul>
+                                        <li v-for="proyecto in semillero.proyectos" :key="proyecto.nombre">
+                                            <strong>{{ proyecto.nombre }}</strong> - {{ proyecto.tipo }} ({{ proyecto.estado }}) en {{ proyecto.campoSaber }}
+                                        </li>
+                                    </ul>
+                                </AccordionTab>
+                                <AccordionTab header="Publicaciones">
+                                    <ul>
+                                        <li v-for="publicacion in semillero.publicaciones" :key="publicacion.titulo">
+                                            <strong>{{ publicacion.titulo }}</strong> ({{ publicacion.anio }}) - {{ publicacion.medio }}
+                                        </li>
+                                    </ul>
+                                </AccordionTab>
                             </Accordion>
                         </TabPanel>
-
-
                     </TabView>
                 </template>
             </Card>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
-
+<script setup>
+import semillero from '../data/dataSemilleros.json'
+</script>
 <template>
   <div class="m-4">
-    <Jrplaneacion/>
+    <Jrplaneacion :semillero="semillero[1]"/>
   </div>
 </template>
