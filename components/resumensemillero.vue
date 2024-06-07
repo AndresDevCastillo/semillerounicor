eb5a2dcc7364244510c54286912df09b51376bcf
joelserran <brandonserrano2208@gmail.com>
2024-06-07 20:22:37 +0000

    Subi el componente de resumen de semillero

---
+++ components/resumensemillero.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+const props = defineProps({
+    nombreSemillero: String,
+    integrantes: Array,
+    mision: String,
+    vision: String,
+    resumen: String,
+})
+</script>
+<template>
+    <div>
+        <Card>
+            <template #title>{{ props.nombreSemillero }}</template>
+            <template #content>
+                <Accordion :activeIndex="0">
+                    <AccordionTab header="Misión">
+                        <p class="m-0">
+                            {{ props.mision }}
+                        </p>
+                    </AccordionTab>
+                    <AccordionTab header="Visión">
+                        <p class="m-0">
+                            {{ props.vision }}
+                        </p>
+                    </AccordionTab>
+                </Accordion>
+
+                <DataTable :value="props.integrantes">
+                    <template #header>
+                        <div class="flex flex-wrap justify-content-end gap-2">
+                            Integrantes
+                        </div>
+                    </template>
+                    <Column field="nombre" header="Nombre"></Column>
+                    <Column field="identificacion" header="Nit"></Column>
+                    <Column field="nivelAcademico" header="Nivel Academico"></Column>
+                </DataTable>
+            </template>
+        </Card>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,11 @@
-<script></script>
+<script setup>
+import semilleros from '../data/semillero.json'
+
+const semillero = semilleros[0]
+
+</script>
 <template>
-  <div></div>
+  <div class="flex justify-center">
+    <Resumensemillero class="w-1/2 m-4" :nombreSemillero="semillero.nombre" :vision="semillero.vision" :mision="semillero.mision" :integrantes="semillero.integrantes.estudiantes"/>
+  </div>
 </template>
