43bfa19b9bf4a8dcd831268ad1ad46b6afe789f1
JoseDanielDurango <jdurangocaballero59@correo.unicordoba.edu.co>
2024-06-11 17:49:48 -0500

    Test

---
+++ pages/semillerocompleto2.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Descripcion from '~/components/Descripcion.vue';
+</script>
+
+<template>
+    <div>
+        <h1>Semillero Completo</h1>
+        <Test2 />
+    </div>
+</template>
