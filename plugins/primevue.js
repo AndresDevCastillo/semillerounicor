b42eb643c19826c24173eb1c5134f8982c473bf8
JoseDanielDurango <jdurangocaballero59@correo.unicordoba.edu.co>
2024-06-18 18:19:43 -0500

    Pull Up Hoe

---
+++ plugins/primevue.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import PrimeVue from 'primevue/config';
+import Avatar from 'primevue/avatar';
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
+import MenuBar from 'primevue/menubar';
+import Menu from 'primevue/menu';
+
+Vue.use(PrimeVue);
+Vue.component('Avatar', Avatar);
+Vue.component('Card', Card);
+Vue.component('Divider', Divider);
+Vue.component('MenuBar', MenuBar);
+Vue.component('Menu', Menu);
