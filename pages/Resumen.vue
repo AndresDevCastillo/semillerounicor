525f638c51281a5d3f2e0351f2de2d7d758d69db
andreslara5 <94642796+andreslara5@users.noreply.github.com>
2024-06-24 11:47:43 -0500

    actualiz
    s

---
+++ pages/Resumen.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ResumenData from '../components/ResumenData.vue';
+import dataSemilleros from '../data/dataSemilleros'
+</script>
+<template>
+    <div>
+      <ResumenData :semillero="dataSemilleros"/>
+    </div>      
+</template> 
